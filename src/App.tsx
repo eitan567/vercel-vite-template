e0c9f4550d2db5bdd93780e221c76191152ead03
eitan567 <eitan2007@gmail.com>
2025-02-09 00:36:15 +0200

    remove unnecessary import of React in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
-// deno-lint-ignore-file ban-ts-comment
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import React from 'react';
 
 function App() {
   const [count, setCount] = useState(0)
