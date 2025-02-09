7dbac0a8411ae3f64225bca5347e870f330bdeb8
eitan567 <eitan2007@gmail.com>
2025-02-09 22:45:13 +0200

    update Vite configuration to specify output directory and remove Jeopardy Genetics game HTML file

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    outDir: 'dist',    
+  },
   plugins: [
     react(),
     tailwindcss(),
-  ],
-  assetsInclude: ['**/*.html'],
+  ]
 })
